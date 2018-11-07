1dd6dc054c6647d33967bdaee70462ca93f5c383
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2018-11-07 10:35:50 +0200

    ru\uk files were updated

--- ci-build.sh
+++ ci-build.sh
@@ -6,7 +6,7 @@ if [ ! -z "$TRAVIS_TAG" ]; then DO=stable; else DO=latest;fi
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
         echo "-----> build for  linux"
-        if [ ! -z "$TRAVIS_TAG" ]; then sed -i 's/1.0.0/'${TRAVIS_TAG}'/g' package.json; else sed -i 's/1.0.0/0.'${TRAVIS_JOB_NUMBER}'/g' package.json; fi
+        if [ ! -z "$TRAVIS_TAG" ]; then sed -i 's/1.0.0/'${TRAVIS_TAG}'/g' package.json; else sed -i 's/1.0.0/1.'${TRAVIS_JOB_NUMBER}'/g' package.json; fi
         docker run --rm \
         --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
         -v ${PWD}:/project \
@@ -16,7 +16,7 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
         /bin/bash -c "npm install && npm run relese-$DO -- --win --linux"
     else
         echo "-----> build for mac"
-        if [ ! -z "$TRAVIS_TAG" ]; then sed -i '' 's/1.0.0/'${TRAVIS_TAG}'/g' package.json; else sed -i '' 's/1.0.0/0.'${TRAVIS_JOB_NUMBER}'/g' package.json; fi
+        if [ ! -z "$TRAVIS_TAG" ]; then sed -i '' 's/1.0.0/'${TRAVIS_TAG}'/g' package.json; else sed -i '' 's/1.0.0/1.'${TRAVIS_JOB_NUMBER}'/g' package.json; fi
         /bin/bash -c "npm install && npm run relese-$DO -- --mac"
 
 fi
