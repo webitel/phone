7eea0b91cb7b21aae1090e16c1fc8261dd738f45
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2018-11-14 16:31:54 +0200

    remove publisher

--- ci-publish.sh
+++ ci-publish.sh
@@ -2,4 +2,5 @@
 
 set -ev
 
+chmod 600 id_rsa
 rsync -azP -e "ssh -p 44022 -i id_rsa -o StrictHostKeyChecking=no" --exclude '*/*/' --include '*' release-builds wphone@cloud-ams1.webitel.com:~/builds/
