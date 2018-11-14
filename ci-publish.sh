0781228898076102fe0824e07098e0649f1f7b94
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2018-11-14 16:56:26 +0200

    exclude binnary

--- ci-publish.sh
+++ ci-publish.sh
@@ -3,4 +3,4 @@
 set -ev
 
 chmod 600 id_rsa
-rsync -azP -e "ssh -p 44022 -i id_rsa -o StrictHostKeyChecking=no" --exclude '*/*/' --include '*' release-builds/ wphone@cloud-ams1.webitel.com:~/builds/
+rsync -azP -e "ssh -p 44022 -i id_rsa -o StrictHostKeyChecking=no" --exclude mac --exclude linux-unpacked --exclude win-unpacked release-builds/ wphone@cloud-ams1.webitel.com:~/builds/
