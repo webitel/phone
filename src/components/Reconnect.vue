06009ba60ea83a91a691fa1e63c186641193fa95
navrotskyj <navrotskyj@gmail.com>
2019-01-17 18:02:49 +0200

    added system bar for reconnect page

--- src/components/Reconnect.vue
+++ src/components/Reconnect.vue
@@ -1,24 +1,40 @@
 <template>
-  <v-container fluid fill-height class="reconnect-page">
-    <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <v-container fluid fill-height>
-          <v-layout align-center justify-center>
-            <v-progress-circular style="text-align: center;" indeterminate :size="140" :width="2" color="warning">
-              Connecting...
-              {{count}}
-            </v-progress-circular>
-          </v-layout>
-        </v-container>
-        <v-container fluid fill-height>
-          <v-layout align-center justify-center>
-            <v-btn flat @click="cancelReconnect" >Cancel</v-btn>
-          </v-layout>
-        </v-container>
+  <div>
+    <v-system-bar app class="lighten-1" height="30px">
+      <span class="drag-zone" style="height: 100%;">
 
-      </v-flex>
-    </v-layout>
-  </v-container>
+      </span>
+      <v-spacer></v-spacer>
+      <div class="system-bar-icons">
+        <a  @click="minimize">
+          <v-icon>remove</v-icon>
+        </a>
+        <a  @click="hide">
+          <v-icon>close</v-icon>
+        </a>
+      </div>
+    </v-system-bar>
+    <v-container fluid fill-height class="reconnect-page">
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-container fluid fill-height>
+            <v-layout align-center justify-center>
+              <v-progress-circular style="text-align: center;" indeterminate :size="140" :width="2" color="warning">
+                Connecting...
+                {{count}}
+              </v-progress-circular>
+            </v-layout>
+          </v-container>
+          <v-container fluid fill-height>
+            <v-layout align-center justify-center>
+              <v-btn flat @click="cancelReconnect" >Cancel</v-btn>
+            </v-layout>
+          </v-container>
+
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -70,6 +86,19 @@
         },
         cancelReconnect() {
           this.$store.dispatch("logout");
+        },
+        minimize() {
+          if (typeof WEBITEL_MINIMALIZE === 'function') {
+            WEBITEL_MINIMALIZE()
+          }
+          //todo
+        },
+
+        hide() {
+          if (typeof WEBITEL_HIDE === 'function') {
+            WEBITEL_HIDE()
+          }
+          //todo
         }
       }
 
